24fa8333ee7f6100a2c914ed45e90c4361fc2548
Chandan Kumar <chandan.kumar@imaginea.com>
2017-04-25 16:37:03 +0530

    test commit; do not merge

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 // Transaction id should be replaced in the file to connect ot the backend
 const Contract = {
-  id: '0xb28c03cf81a9dd001677102fed1b07b6d7b6d4f5'
+  id: '0x3835d833a0f9270efe64a00c83d156252b97f93d'
 }
 
 export default Contract
